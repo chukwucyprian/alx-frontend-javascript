482fc0f4f1cf3b260126a68cadc0c5b638060af0
chukwucyprian <chukwucyprian262@gmail.com>
2024-01-02 22:40:17 +0100

    additional tasks

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -6,8 +6,8 @@ export default function taskBlock(trueOrFalse) {
   if (trueOrFalse) {
     task = true;
     task2 = false;
-    }
+  }
   
-    return [task, task2];
+  return [task, task2];
 }
   
