4dbdff6636a47029278dfde0639e9d0f538960fb
chukwucyprian <chukwucyprian262@gmail.com>
2024-01-04 01:34:28 +0100

    tasks2

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,14 +1,8 @@
-// 0-promise.js
 function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      // Simulating an API call that takes some time
-      setTimeout(() => {
-        // Assuming the API call was successful, resolving the Promise with some data
-        resolve({ message: "Data from the API" });
-        // In a real scenario, you might fetch data from an actual API
-      }, 2000); // Simulating a delay of 2 seconds
-    });
-  }
-  
-  export default getResponseFromAPI;
-  
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ message: 'Data from the API' });
+    }, 2000);
+  });
+}
+export default getResponseFromAPI;
