5547437f46aab2c27d49a2e024e3eddee4e5970f
chukwucyprian <chukwucyprian262@gmail.com>
2024-01-05 06:08:49 +0100

    more

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,57 +1,52 @@
-// 2-hbtn_course.js
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = this.validateString(name, 'Name');
-    this._length = this.validateNumber(length, 'Length');
-    this._students = this.validateStudents(students);
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = name;
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    } else {
+      this._length = length;
+    }
+    if (!Array.isArray(students)) {
+      throw new TypeError('must be a array');
+    } else {
+      this._students = students;
+    }
   }
-  // Getter and setter for 'name'
 
   get name() {
     return this._name;
   }
 
-  set name(newName) {
-    this._name = this.validateString(newName, 'Name');
+  set name(na) {
+    if (typeof na !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = na;
   }
-  // Getter and setter for 'length'
 
   get length() {
     return this._length;
   }
 
-  set length(newLength) {
-    this._length = this.validateNumber(newLength, 'Length');
+  set length(zise) {
+    if (typeof zise !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = zise;
   }
 
-  // Getter and setter for 'students'
   get students() {
     return this._students;
   }
 
-  set students(newStudents) {
-    this._students = this.validateStudents(newStudents);
-  }
-
-  // Validation functions
-  validateString(value, fieldName) {
-    if (typeof value !== 'string') {
-      throw new TypeError(`${fieldName} must be a string`);
+  set students(stu) {
+    if (!Array.isArray(stu)) {
+      throw new TypeError('must be a array');
     }
-    return value;
-  }
-
-  validateNumber(value, fieldName) {
-    if (typeof value !== 'number' || Number.isNaN(value)) {
-      throw new TypeError(`${fieldName} must be a number`);
-    }
-    return value;
-  }
-
-  validateStudents(students) {
-    if (!Array.isArray(students) || students.some((student) => typeof student !== 'string')) {
-      throw new TypeError('Students must be an array of strings');
-    }
-    return students;
+    this._students = stu;
   }
 }
--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,16 @@
-// 5-building.js
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
-  // Getter for 'sqft'
   get sqft() {
     return this._sqft;
   }
 
-  // Abstract method
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+  set sqft(value) {
+    this._sqft = value;
   }
 }
