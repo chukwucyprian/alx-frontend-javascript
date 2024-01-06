4598a19a68ca564ba558949d39a9f467b408201b
chukwucyprian <chukwucyprian262@gmail.com>
2024-01-06 08:24:02 +0100

    correction

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,22 +1,13 @@
-// 100-evcar.js (EVCar class)
-
-import Car from './10-car.js';
+/* eslint-disable */
+import Car from './10-car';
 
 export default class EVCar extends Car {
-  constructor(brand, motor, color, range) {
-    super();
-    this._brand = brand;
-    this._motor = motor;
-    this._color = color;
-    this._range = range;
-  }
+	constructor(brand, motor, color, range) {
+		super(brand, motor, color);
+		this._range = range;
+	}
 
-  cloneCar() {
-    const { _brand, _motor, _color, _range } = this;
-    const car = new Car();
-    car._brand = _brand;
-    car._motor = _motor;
-    car._color = _color;
-    return car;
-  }
+	cloneCar() {
+		return new Car();
+	}
 }
