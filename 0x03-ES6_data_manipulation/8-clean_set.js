09331fbfcc4cfb221828e79dad06201b2565df43
chukwucyprian <chukwucyprian262@gmail.com>
2024-01-08 23:10:24 +0100

    correction

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,11 @@
-function cleanSet(set, startString) {
-  const filteredValues = [...set].filter((value) => value.startsWith(startString));
-  return filteredValues.map((value) => value.slice(startString.length)).join('-');
-}
+const cleanSet = (set, startString) => {
+  if (startString === undefined || startString.length === 0) {
+    return '';
+  }
+  return [...set]
+    .filter((parametro) => (parametro !== undefined ? parametro.startsWith(startString) : ''))
+    .map((parametro) => (parametro !== undefined ? parametro.slice(startString.length) : ''))
+    .join('-');
+};
 
 export default cleanSet;
