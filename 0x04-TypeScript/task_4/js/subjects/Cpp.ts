5950ded8e22dbbf3e3d758bb2de8b03b4683441f
chukwucyprian <chukwucyprian262@gmail.com>
2024-01-10 20:30:50 +0100

    task_4

---
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -0,0 +1,17 @@
+// task_4/js/subjects/Cpp.ts
+
+namespace Subjects {
+    export class Cpp extends Subject {
+      getRequirements(): string {
+        return "Here is the list of requirements for Cpp";
+      }
+  
+      getAvailableTeacher(): string {
+        if (this._teacher?.experienceTeachingC) {
+          return `Available Teacher: ${this._teacher.firstName}`;
+        } else {
+          return "No available teacher";
+        }
+      }
+    }
+}
